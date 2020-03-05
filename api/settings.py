f246264254d831cf24077529a2cf3d99e153de91
Ali Yamini <d.aliyamini@gmail.com>
2020-03-05 16:01:57 +0330

    Adding postgre database

--- api/settings.py
+++ api/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testapi',
+        'USER': 'ali_ya',
+        'PASSWORD': 'SXzI2SfB',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
