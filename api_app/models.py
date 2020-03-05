4e94922bd6e934dabb9e45db5dd155261991e445
Ali Yamini <d.aliyamini@gmail.com>
2020-03-05 16:47:53 +0330

    Adding Name model

--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Name(models.Model):
+    """This class represents the loglist model."""
+    name = models.TextField(null=True, default="Not defined in message")
+
+    def __str__(self):
+        self.name;
--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.core import serializers
+from django.http import HttpResponse, JsonResponse, HttpRequest, request
 
 # Create your views here.
+def hello(request):
+    pass
