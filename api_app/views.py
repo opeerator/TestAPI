5d887c501f31482a5176af590cb795de4f315ce9
Ali Yamini <d.aliyamini@gmail.com>
2020-03-05 17:41:34 +0330

    completing hello function

--- api_app/views.py
+++ api_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.core import serializers
-from django.http import HttpResponse, JsonResponse, HttpRequest, request
+from django.http import HttpResponse, HttpRequest, request
 from .models import Name
 from django.views.decorators.csrf import csrf_exempt
 
@@ -11,7 +10,7 @@ def hello(request):
         client_name = request.GET['name']
         name_instance = Name()
         name_instance.name = client_name
-        return HttpResponse(name_instance.name)
         name_instance.save()
+        return HttpResponse('{meta:{status:200}data:{message: hello, '  + client_name + '}}')
     except:
         return HttpResponse("There is a problem with that input!")
