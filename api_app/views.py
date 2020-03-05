d23c36052a6872c770da58ecc3f8115580b60f74
Ali Yamini <d.aliyamini@gmail.com>
2020-03-05 18:13:25 +0330

    Database ignore for tests

--- api_app/views.py
+++ api_app/views.py
@@ -8,9 +8,9 @@ from django.views.decorators.csrf import csrf_exempt
 def hello(request):
     try:
         client_name = request.GET['name']
-        name_instance = Name()
-        name_instance.name = client_name
-        name_instance.save()
+        # name_instance = Name()
+        # name_instance.name = client_name
+        # name_instance.save()
         return HttpResponse('{meta:{status:200}data:{message: hello, '  + client_name + '}}')
     except:
         return HttpResponse("There is a problem with that input!")
