148ce88708f17b95509ab6b51b52ca11514e912c
Ali Yamini <mail@aliyamini.ir>
2020-03-18 00:40:50 +0330

    Add new test cases

--- locustfile.py
+++ locustfile.py
@@ -1,13 +1,5 @@
 from locust import HttpLocust, TaskSet, task, between
 
-def fibo_example_1(self):
-    self.client.get("/api/fibonacci?index=0")
-def fibo_example_2(self):
-    self.client.get("/api/fibonacci?index=1")
-def fibo_example_3(self):
-    self.client.get("/api/fibonacci?index=5")
-def fibo_example_4(self):
-    self.client.get("/api/fibonacci?index=8")
 def fibo_example_5(self):
     self.client.get("/api/fibonacci?index=13")
 def fibo_example_6(self):
@@ -20,20 +12,46 @@ def fibo_example_9(self):
     self.client.get("/api/fibonacci?index=89")
 def fibo_example_10(self):
     self.client.get("/api/fibonacci?index=144")
+def fibo_example_11(self):
+    self.client.get("/api/fibonacci?index=233")
+def fibo_example_12(self):
+    self.client.get("/api/fibonacci?index=377")
+def fibo_example_13(self):
+    self.client.get("/api/fibonacci?index=610")
+def fibo_example_14(self):
+    self.client.get("/api/fibonacci?index=987")
+def fibo_example_15(self):
+    self.client.get("/api/fibonacci?index=1597")
+def fibo_example_16(self):
+    self.client.get("/api/fibonacci?index=2584")
+def fibo_example_17(self):
+    self.client.get("/api/fibonacci?index=4181")
+def fibo_example_18(self):
+    self.client.get("/api/fibonacci?index=6765")
+def fibo_example_19(self):
+    self.client.get("/api/fibonacci?index=10946")
+def fibo_example_20(self):
+    self.client.get("/api/fibonacci?index=17711")
     
 
 class WebsiteTasks(TaskSet):
     tasks = [
-        fibo_example_1,
-        fibo_example_2,
-        fibo_example_3,
-        fibo_example_4,
         fibo_example_5,
         fibo_example_6,
         fibo_example_7,
         fibo_example_8,
         fibo_example_9,
-        fibo_example_10
+        fibo_example_10,
+        fibo_example_11,
+        fibo_example_12,
+        fibo_example_13,
+        fibo_example_14,
+        fibo_example_15,
+        fibo_example_16,
+        fibo_example_17,
+        fibo_example_18,
+        fibo_example_19,
+        fibo_example_20
     ]
 
 class WebsiteUser(HttpLocust):
